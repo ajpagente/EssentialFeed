a17497f6e739f061465ca7c2131d9177adf60ac2
ajepagen <alvin-john.pagente@thalesgroup.com>
2023-02-02 22:30:17 +0800

    Create FeedItem model

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Alvin John Pagente on 2/2/23.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}

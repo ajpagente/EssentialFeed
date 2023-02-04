8659181a8c0d47d718577786e00f08a52e48fc14
ajepagen <alvin-john.pagente@thalesgroup.com>
2023-02-04 09:13:28 +0800

    Move RemoteFeedLoader components to production. Change RemoteFeedLoader to a class as it has behaviour.

---
+++ EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -0,0 +1,27 @@
+//
+//  RemoteFeedLoader.swift
+//  EssentialFeed
+//
+//  Created by Alvin John Pagente on 4/2/23.
+//
+
+import Foundation
+
+public protocol HTTPClient {
+    func get(from url: URL)
+}
+
+public final class RemoteFeedLoader {
+    private let url: URL
+    private let client: HTTPClient
+    
+    public init(url: URL, client: HTTPClient) {
+        self.url = url
+        self.client = client
+    }
+    
+    public func load() {
+        client.get(from: url)
+    }
+}
+
--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -6,19 +6,7 @@
 //
 
 import XCTest
-
-struct RemoteFeedLoader {
-    let url: URL
-    let client: HTTPClient
-    
-    func load() {
-        client.get(from: url)
-    }
-}
-
-protocol HTTPClient {
-    func get(from url: URL)
-}
+@testable import EssentialFeed
 
 class RemoteFeedLoaderTests: XCTestCase {
 

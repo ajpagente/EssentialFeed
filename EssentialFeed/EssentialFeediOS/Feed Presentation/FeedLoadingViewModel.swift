e1abc45ed3db78fe67f2448e9c68ffb208b6ff91
ajpagente <alvininsg@gmail.com>
2023-03-19 23:07:27 +0800

    Move ViewModels to separate files.

---
+++ EssentialFeed/EssentialFeediOS/Feed Presentation/FeedLoadingViewModel.swift
@@ -0,0 +1,10 @@
+//
+//  FeedLoadingViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by Alvin Johnn Pagente on 19/3/23.
+//
+
+struct FeedLoadingViewModel {
+    let isLoading: Bool
+}
---
+++ EssentialFeed/EssentialFeediOS/Feed Presentation/FeedViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  FeedViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by Alvin Johnn Pagente on 19/3/23.
+//
+
+import EssentialFeed
+
+struct FeedViewModel {
+    let feed: [FeedImage]
+}
+
+

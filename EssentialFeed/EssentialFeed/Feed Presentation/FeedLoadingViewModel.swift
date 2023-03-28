87bf8eba63ba96e00717eeedf451e07e2b62c331
ajpagente <alvininsg@gmail.com>
2023-03-28 13:34:35 +0800

    Move `FeedLoadingViewModel` to separate file.

---
+++ EssentialFeed/EssentialFeed/Feed Presentation/FeedLoadingViewModel.swift
@@ -0,0 +1,10 @@
+//
+//  FeedLoadingViewModel.swift
+//  EssentialFeed
+//
+//  Created by Alvin John Pagente on 28/3/23.
+//
+
+public struct FeedLoadingViewModel {
+    public let isLoading: Bool
+}

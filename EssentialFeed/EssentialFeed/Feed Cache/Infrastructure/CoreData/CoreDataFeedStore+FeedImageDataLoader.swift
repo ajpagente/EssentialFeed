93b19375ae270ef90b06c6c59cf1f794856c4408
ajpagente <alvininsg@gmail.com>
2023-04-01 22:34:52 +0800

    CoreDataFeedStore.retrieveImageData delivers stored data when theres an image with a matching URL in the store

--- EssentialFeed/EssentialFeed/Feed Cache/Infrastructure/CoreData/CoreDataFeedStore+FeedImageDataLoader.swift
+++ EssentialFeed/EssentialFeed/Feed Cache/Infrastructure/CoreData/CoreDataFeedStore+FeedImageDataLoader.swift
@@ -11,11 +11,10 @@ extension CoreDataFeedStore: FeedImageDataStore {
 
     public func insert(_ data: Data, for url: URL, completion: @escaping (FeedImageDataStore.InsertionResult) -> Void) {
         perform { context in
-            completion(Result {
-                try ManagedFeedImage.first(with: url, in: context)
-                    .map { $0.data = data }
-                    .map(context.save)
-            })
+            guard let image = try? ManagedFeedImage.first(with: url, in: context) else { return }
+            
+            image.data = data
+            try? context.save()
         }
     }
 

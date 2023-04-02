fbdf9f7517ed0d57a16103eb4e21253e488f2cb2
ajpagente <alvininsg@gmail.com>
2023-04-02 14:58:40 +0800

    `LocalFeedLoader.validateCache` succeeds on empty cache

--- EssentialFeed/EssentialFeedTests/Feed Cache/ValidateFeedCacheUseCaseTest.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/ValidateFeedCacheUseCaseTest.swift
@@ -87,6 +87,14 @@ class ValidateFeedCacheUseCaseTest: XCTestCase {
             store.completeDeletionSuccessfully()
         })
     }
+    
+    func test_validateCache_succeedsOnEmptyCache() {
+        let (sut, store) = makeSUT()
+
+        expect(sut, toCompleteWith: .success(()), when: {
+            store.completeRetrievalWithEmptyCache()
+        })
+    }
 
     func test_validateCache_doesNotDeleteInvalidCacheAfterSUTInstanceHasBeenDeallocated() {
         let store = FeedStoreSpy()

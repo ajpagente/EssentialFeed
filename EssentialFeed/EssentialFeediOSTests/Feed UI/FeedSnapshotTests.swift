9b0f47ac4d78c1c6cef6c66b4c5e0e5e57d6b502
ajpagente <alvininsg@gmail.com>
2023-04-17 21:10:45 +0800

    Replace snapshots for feed with content and feed with failed image loading as there were updates to the storyboard and uncommented tests. Temporarily commented out `test_feedWithErrorMessage` as the UI is not clear at this point.

--- EssentialFeed/EssentialFeediOSTests/Feed UI/FeedSnapshotTests.swift
+++ EssentialFeed/EssentialFeediOSTests/Feed UI/FeedSnapshotTests.swift
@@ -20,32 +20,32 @@ class FeedSnapshotTests: XCTestCase {
         assert(snapshot: sut.snapshot(for: .iPhone8(style: .dark)), named: "EMPTY_FEED_dark")
     }
     
-    //    func test_feedWithContent() {
-    //        let sut = makeSUT()
-    //
-    //        sut.display(feedWithContent())
-    //
-    //        assert(snapshot: sut.snapshot(for: .iPhone8(style: .light)), named: "FEED_WITH_CONTENT_light")
-    //        assert(snapshot: sut.snapshot(for: .iPhone8(style: .dark)), named: "FEED_WITH_CONTENT_dark")
-    //    }
-    
-    func test_feedWithErrorMessage() {
+    func test_feedWithContent() {
         let sut = makeSUT()
         
-        sut.display(.error(message: "This is a\nmulti-line\nerror message"))
+        sut.display(feedWithContent())
         
-        assert(snapshot: sut.snapshot(for: .iPhone8(style: .light)), named: "FEED_WITH_ERROR_MESSAGE_light")
-        assert(snapshot: sut.snapshot(for: .iPhone8(style: .dark)), named: "FEED_WITH_ERROR_MESSAGE_dark")
+        assert(snapshot: sut.snapshot(for: .iPhone8(style: .light)), named: "FEED_WITH_CONTENT_light")
+        assert(snapshot: sut.snapshot(for: .iPhone8(style: .dark)), named: "FEED_WITH_CONTENT_dark")
     }
     
-    //    func test_feedWithFailedImageLoading() {
-    //        let sut = makeSUT()
-    //
-    //        sut.display(feedWithFailedImageLoading())
-    //
-    //        assert(snapshot: sut.snapshot(for: .iPhone8(style: .light)), named: "FEED_WITH_FAILED_IMAGE_LOADING_light")
-    //        assert(snapshot: sut.snapshot(for: .iPhone8(style: .dark)), named: "FEED_WITH_FAILED_IMAGE_LOADING_dark")
-    //    }
+//    func test_feedWithErrorMessage() {
+//        let sut = makeSUT()
+//
+//        sut.display(.error(message: "This is a\nmulti-line\nerror message"))
+//
+//        assert(snapshot: sut.snapshot(for: .iPhone8(style: .light)), named: "FEED_WITH_ERROR_MESSAGE_light")
+//        assert(snapshot: sut.snapshot(for: .iPhone8(style: .dark)), named: "FEED_WITH_ERROR_MESSAGE_dark")
+//    }
+    
+    func test_feedWithFailedImageLoading() {
+        let sut = makeSUT()
+        
+        sut.display(feedWithFailedImageLoading())
+        
+        assert(snapshot: sut.snapshot(for: .iPhone8(style: .light)), named: "FEED_WITH_FAILED_IMAGE_LOADING_light")
+        assert(snapshot: sut.snapshot(for: .iPhone8(style: .dark)), named: "FEED_WITH_FAILED_IMAGE_LOADING_dark")
+    }
     
     // MARK: - Helpers
     private func makeSUT() -> ListViewController {

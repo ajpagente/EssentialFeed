750ec10e5cefeba75251edeec2372451d2b71e50
ajpagente <alvininsg@gmail.com>
2023-04-22 13:08:29 +0800

    Extract endpoint URL creation to the API modules.

---
+++ EssentialFeed/EssentialFeedTests/Feed API/FeedEndpointsTest.swift
@@ -0,0 +1,22 @@
+//
+//  FeedEndpointsTest.swift
+//  EssentialFeedTests
+//
+//  Created by Alvin John Pagente on 22/4/23.
+//
+
+import XCTest
+import EssentialFeed
+
+class FeedEndpointTests: XCTestCase {
+
+    func test_feed_endpointURL() {
+        let baseURL = URL(string: "http://base-url.com")!
+
+        let received = FeedEndpoint.get.url(baseURL: baseURL)
+        let expected = URL(string: "http://base-url.com/v1/feed")!
+
+        XCTAssertEqual(received, expected)
+    }
+
+}
